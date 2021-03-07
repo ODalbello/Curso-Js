8124b480b62d0f083c545fb7df8a9accea1f7c97
Luiz Dalbello <luiz.dalbello@outlook.com>
2021-03-07 11:35:53 -0300

    adding the second gulp exercice to the GULP module

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,10 +1,31 @@
 const gulp = require('gulp')
 // const { series } = require('gulp') Pode ser feito das duas maneiras
-const series = gulp.series
+// const series = gulp.series
+const { series, parallel } = require('gulp')
 
-function copiar(cb) {
-    console.log('Tarefa de copiar!')
+const antes1 = cb => {
+    console.log('Tarefa Antes 1')
     return cb()
 }
 
-module.exports.default = series(copiar)
+const antes2 = cb => {
+    console.log('Tarefa Antes 2')
+    return cb()
+}
+
+function copiar(cb) {
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+        .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim!')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim
+)
